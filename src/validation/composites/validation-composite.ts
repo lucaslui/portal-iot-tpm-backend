7697e8ab7cb2d4ff47ab70252ddc870e8f32cf3e
Lucas Lui Motta <lucasluimotta@gmail.com>
2021-02-19 11:40:12 -0300

    refactor: remove strict return

--- src/validation/composites/validation-composite.ts
+++ src/validation/composites/validation-composite.ts
@@ -3,7 +3,7 @@ import { Validation } from '@/presentation/protocols/validation'
 export class ValidationComposite implements Validation {
   constructor (private readonly validations: Validation[]) { }
 
-  validate (input: any): Error | undefined {
+  validate (input: any): Error {
     for (const validation of this.validations) {
       const error = validation.validate(input)
       if (error) {
--- src/validation/validators/required-fields-validation.ts
+++ src/validation/validators/required-fields-validation.ts
@@ -4,7 +4,7 @@ import { Validation } from '@/presentation/protocols/validation'
 export class RequiredFieldsValidation implements Validation {
   constructor (private readonly fieldName: string) { }
 
-  validate (input: any): Error | undefined {
+  validate (input: any): Error {
     if (!input[this.fieldName]) {
       return new MissingParamError(this.fieldName)
     }
