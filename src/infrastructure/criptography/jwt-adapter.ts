3ffb1fa3aae30bc9dc4219328424c15e0956821a
Lucas Lui Motta <lucasluimotta@gmail.com>
2021-02-19 00:12:30 -0300

    feat: add BcryptAdapter, JwtAdapter and AccountMongoRepository

---
+++ src/infrastructure/criptography/jwt-adapter.ts
@@ -0,0 +1,17 @@
+import { Decrypter } from '@/data/protocols/cryptograph/decrypter'
+import { Encrypter } from '@/data/protocols/cryptograph/encrypter'
+import jwt from 'jsonwebtoken'
+
+export class JwtAdapter implements Encrypter, Decrypter {
+  constructor (private readonly secret: string) {}
+
+  async encrypt (value: string): Promise<string> {
+    const accessToken = await jwt.sign({ id: value }, this.secret)
+    return accessToken
+  }
+
+  async decrypt (token: string): Promise<string | object> {
+    const decodedToken = await jwt.verify(token, this.secret)
+    return decodedToken
+  }
+}
