f29811d1f5e074d8e21154f5427050774fb32400
Lucas Lui Motta <lucasluimotta@gmail.com>
2021-02-25 18:58:06 -0300

    refactor: variable names

--- src/data/services/auth/db-create-user.ts
+++ src/data/services/auth/db-create-user.ts
@@ -1,6 +1,6 @@
 import { Hasher } from '@/data/protocols/cryptograph/hasher'
-import { CreateUserRepository } from '@/data/protocols/database/create-user-repository'
-import { LoadUserByEmailRepository } from '@/data/protocols/database/load-user-by-email-repository'
+import { CreateUserRepository } from '@/data/protocols/database/auth/create-user-repository'
+import { LoadUserByEmailRepository } from '@/data/protocols/database/auth/load-user-by-email-repository'
 import { UserModel } from '@/domain/entities/user'
 import { CreateUser, CreateUserParamsModel } from '@/domain/usecases/auth/create-user'
 
@@ -8,12 +8,12 @@ export class DbCreateUser implements CreateUser {
   constructor (
     private readonly hasher: Hasher,
     private readonly createUserRepository: CreateUserRepository,
-    private readonly loadAccountByEmailRepository: LoadUserByEmailRepository
+    private readonly loadUserByEmailRepository: LoadUserByEmailRepository
   ) { }
 
   async create (createUserParams: CreateUserParamsModel): Promise<UserModel> {
-    const account = await this.loadAccountByEmailRepository.loadByEmail(createUserParams.email)
-    if (!account) {
+    const user = await this.loadUserByEmailRepository.loadByEmail(createUserParams.email)
+    if (!user) {
       const hashedPassword = await this.hasher.hash(createUserParams.password)
       const newAccount = await this.createUserRepository.create(Object.assign({}, createUserParams, { password: hashedPassword }))
       return newAccount
--- src/data/services/auth/db-load-user-by-token.ts
+++ src/data/services/auth/db-load-user-by-token.ts
@@ -1,20 +1,20 @@
 import { Decrypter } from '@/data/protocols/cryptograph/decrypter'
-import { LoadUserByTokenRepository } from '@/data/protocols/database/load-user-by-token-repository'
+import { LoadUserByTokenRepository } from '@/data/protocols/database/auth/load-user-by-token-repository'
 import { UserModel } from '@/domain/entities/user'
 import { LoadUserByToken } from '@/domain/usecases/auth/load-user-by-token'
 
 export class DbLoadUserByToken implements LoadUserByToken {
   constructor (
     private readonly decrypter: Decrypter,
-    private readonly loadAccountByTokenRepository: LoadUserByTokenRepository
+    private readonly loadUserByTokenRepository: LoadUserByTokenRepository
   ) {}
 
   async load (accessToken: string, role?: string): Promise<UserModel> {
     const decodedToken = await this.decrypter.decrypt(accessToken)
     if (decodedToken) {
-      const account = await this.loadAccountByTokenRepository.loadByToken(accessToken, role)
-      if (account) {
-        return account
+      const user = await this.loadUserByTokenRepository.loadByToken(accessToken, role)
+      if (user) {
+        return user
       }
     }
     return null
