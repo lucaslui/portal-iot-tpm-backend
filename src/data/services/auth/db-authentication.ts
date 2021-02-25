86fdc3cefc0aba0fa0753b5effc5a5564fb5490a
Lucas Lui Motta <lucasluimotta@gmail.com>
2021-02-25 18:57:40 -0300

    refactor: variable names

--- src/data/services/auth/db-authentication.ts
+++ src/data/services/auth/db-authentication.ts
@@ -1,24 +1,24 @@
 import { Encrypter } from '@/data/protocols/cryptograph/encrypter'
 import { HashComparer } from '@/data/protocols/cryptograph/hash-comparer'
-import { LoadUserByEmailRepository } from '@/data/protocols/database/load-user-by-username-repository'
-import { UpdateAccessTokenRepository } from '@/data/protocols/database/update-access-token-repository'
+import { LoadUserByEmailRepository } from '@/data/protocols/database/auth/load-user-by-username-repository'
+import { UpdateAccessTokenRepository } from '@/data/protocols/database/auth/update-access-token-repository'
 import { Authentication, AuthenticationModel } from '@/domain/usecases/auth/authentication'
 
 export class DbAuthentication implements Authentication {
   constructor (
-    private readonly loadAccountByUsernameRepository: LoadUserByEmailRepository,
+    private readonly loadUserByEmailRepository: LoadUserByEmailRepository,
     private readonly hashComparer: HashComparer,
     private readonly encrypter: Encrypter,
     private readonly updateAccessTokenRepository: UpdateAccessTokenRepository
   ) {}
 
   async auth (authentication: AuthenticationModel): Promise<string> {
-    const account = await this.loadAccountByUsernameRepository.loadByEmail(authentication.email)
-    if (account) {
-      const isAuthorized = await this.hashComparer.compare(authentication.password, account.password)
+    const user = await this.loadUserByEmailRepository.loadByEmail(authentication.email)
+    if (user) {
+      const isAuthorized = await this.hashComparer.compare(authentication.password, user.password)
       if (isAuthorized) {
-        const accessToken = await this.encrypter.encrypt(account.id)
-        await this.updateAccessTokenRepository.updateAccessToken(account.id, accessToken)
+        const accessToken = await this.encrypter.encrypt(user.id)
+        await this.updateAccessTokenRepository.updateAccessToken(user.id, accessToken)
         return accessToken
       }
     }
