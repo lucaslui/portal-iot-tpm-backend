8afa2e80c5fa15cd0ac38c3ad54b389919a3f8d2
Lucas Lui Motta <lucasluimotta@gmail.com>
2021-02-27 11:26:34 -0300

    feat: add noContent and refactor

--- src/presentation/helpers/http-helper.ts
+++ src/presentation/helpers/http-helper.ts
@@ -2,26 +2,6 @@ import { ServerError } from '../errors/server-error'
 import { UnauthorizedError } from '../errors/unauthorized-error'
 import { HttpResponse } from '../protocols/http'
 
-export const badRequest = (error: Error): HttpResponse => ({
-  statusCode: 400,
-  body: error
-})
-
-export const forbidden = (error: Error): HttpResponse => ({
-  statusCode: 403,
-  body: error
-})
-
-export const unauthorized = (): HttpResponse => ({
-  statusCode: 401,
-  body: new UnauthorizedError()
-})
-
-export const serverError = (error: Error): HttpResponse => ({
-  statusCode: 500,
-  body: new ServerError(error.stack)
-})
-
 export const ok = (data: any): HttpResponse => ({
   statusCode: 200,
   body: data
@@ -30,4 +10,29 @@ export const ok = (data: any): HttpResponse => ({
 export const noContent = (): HttpResponse => ({
   statusCode: 204,
   body: null
+})
+
+export const badRequest = (error: Error): HttpResponse => ({
+  statusCode: 400,
+  body: error
+})
+
+export const unauthorized = (): HttpResponse => ({
+  statusCode: 401,
+  body: new UnauthorizedError()
+})
+
+export const forbidden = (error: Error): HttpResponse => ({
+  statusCode: 403,
+  body: error
+})
+
+export const notFound = (): HttpResponse => ({
+  statusCode: 404,
+  body: null
+})
+
+export const serverError = (error: Error): HttpResponse => ({
+  statusCode: 500,
+  body: new ServerError(error.stack)
 })
