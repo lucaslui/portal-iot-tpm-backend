8e6519f7e6594b83a63709c1df23108b25d29f6f
lucaslui <llui@tractian.com>
2023-12-17 21:29:34 -0300

    chore: add dotenv

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -1,3 +1,5 @@
+import 'dotenv/config'
+
 export default {
   mongoUrl: process.env.MONGO_URL || 'mongodb://localhost:27017/blog-database?readPreference=primary&appname=MongoDB%20Compass&ssl=false',
   port: process.env.PORT || 5050,
