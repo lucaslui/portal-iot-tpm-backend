7be776791c1f73c4572492d19ffce8c73af9e54e
Lucas Lui Motta <lucasluimotta@gmail.com>
2024-03-05 16:28:51 -0300

    feat: disable signup for improve security

--- src/configuration/routes/auth-routes.ts
+++ src/configuration/routes/auth-routes.ts
@@ -1,13 +1,13 @@
 /* eslint-disable @typescript-eslint/no-misused-promises */
 import { adaptRoute } from '../adapters/express-route-adapter'
 import { makeLoginController } from '../factories/controllers/auth/login-controller-factory'
-import { makeSignUpController } from '../factories/controllers/auth/signup-controller-factory'
+// import { makeSignUpController } from '../factories/controllers/auth/signup-controller-factory'
 
 import { Router } from 'express'
 
 const router = Router()
 
 router.post('/login', adaptRoute(makeLoginController()))
-router.post('/signup', adaptRoute(makeSignUpController()))
+// router.post('/signup', adaptRoute(makeSignUpController()))
 
 export { router as authRouter }
