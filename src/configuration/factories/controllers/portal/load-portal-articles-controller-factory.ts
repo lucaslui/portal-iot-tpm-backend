9f53a17beadc9ba97957ef9d0dbca903538d56ca
Lucas Lui Motta <lucasluimotta@gmail.com>
2024-07-08 14:57:04 -0300

    fix: new load portal articles

--- src/configuration/factories/controllers/portal/load-portal-articles-controller-factory.ts
+++ src/configuration/factories/controllers/portal/load-portal-articles-controller-factory.ts
@@ -1,9 +1,9 @@
 import { Controller } from '@/application/protocols/controller'
 import { makeLogControllerDecorator } from '../../decorators/log-controller-decorator-factory'
-import { makeDbLoadArticles } from '../../usecases/article/db-load-articles-factory'
 import { LoadPortalArticlesController } from '@/application/controllers/portal/load-portal-articles-controller'
+import { makeDbLoadPortalArticles } from '@/configuration/factories/usecases/portal/db-load-portal-articles-factory'
 
 export const makeLoadPortalArticlesController = (): Controller => {
-  const loadPortalArticlesController = new LoadPortalArticlesController(makeDbLoadArticles())
+  const loadPortalArticlesController = new LoadPortalArticlesController(makeDbLoadPortalArticles())
   return makeLogControllerDecorator(loadPortalArticlesController)
 }
--- src/configuration/factories/usecases/portal/db-load-portal-articles-factory.ts
+++ src/configuration/factories/usecases/portal/db-load-portal-articles-factory.ts
@@ -1,8 +1,8 @@
-import { LoadArticles } from '@/usecases/boundaries/inputs/article/load-articles'
 import { PortalMongoRepository } from '@/infrastructure/database/mongodb/portal-mongo-repository'
+import { LoadPortalArticles } from '@/usecases/boundaries/inputs/portal/load-portal-articles'
 import { DbLoadPortalArticles } from '@/usecases/interactors/portal/db-load-portal-articles'
 
-export const makeDbPortalLoadArticles = (): LoadArticles => {
+export const makeDbLoadPortalArticles = (): LoadPortalArticles => {
   const portalMongoRepository = new PortalMongoRepository()
   return new DbLoadPortalArticles(portalMongoRepository)
 }
